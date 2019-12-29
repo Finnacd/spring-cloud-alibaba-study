cb39e7d91511da57a978515f6052b5a0d7603538
Tyrone <shanglishuai@sina.cn>
2019-12-29 11:24:26 +0800

    增加日志说明

--- sca-dependences/sca-account/sca-account-service/src/main/java/cn/tyrone/sca/account/service/AccountServiceImpl.java
+++ sca-dependences/sca-account/sca-account-service/src/main/java/cn/tyrone/sca/account/service/AccountServiceImpl.java
@@ -1,11 +1,10 @@
 package cn.tyrone.sca.account.service;
 
-import org.apache.dubbo.config.annotation.Service;
-
-@Service
+@org.apache.dubbo.config.annotation.Service
 public class AccountServiceImpl implements IAccountService {
 
     public Object register(String name) {
         return name;
     }
+
 }
